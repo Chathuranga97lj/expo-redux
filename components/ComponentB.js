602d5c6be6fc17a320e4a61557d84dfd77c4880c
Chathuranga97lj <chathuit97@gmail.com>
2022-04-18 16:29:51 +0530

    final change

--- components/ComponentB.js
+++ components/ComponentB.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'; // useDispatct for button function
+import { increment, decrement } from '../store/action';
+
 
 const ComponentB = () => {
+
+  const counter = useSelector(state => state)
+
+  const dispatch = useDispatch();
+
   return (
     <View>
-      <Text style={{fontSize: 30}}>ComponentB</Text>
+      <Text style={{fontSize: 30}}>ComponentB : {counter}</Text>
+      <Button title='+' onPress={() => dispatch(increment())}/>
+      <Button title='-' onPress={() => dispatch(decrement())}/>
     </View>
   )
 }
